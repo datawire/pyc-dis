f75711e61d37417905622b7c71e8bc5b58cecaaa
Luke Shumaker <lukeshu@datawire.io>
2021-12-22 10:48:50 -0700

    initial commit

---
+++ dis.go
@@ -0,0 +1,293 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/datawire/dlib/derror"
+)
+
+func main() {
+	if err := Disassemble(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: error: %v\n", os.Args[0], err)
+		os.Exit(1)
+	}
+}
+
+func read_u32le(in io.Reader) uint32 {
+	var ret uint32
+	if err := binary.Read(in, binary.LittleEndian, &ret); err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+func read_u64le(in io.Reader) uint64 {
+	var ret uint64
+	if err := binary.Read(in, binary.LittleEndian, &ret); err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+func read_i32le(in io.Reader) int32 {
+	var ret int32
+	if err := binary.Read(in, binary.LittleEndian, &ret); err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+func read_i64le(in io.Reader) int64 {
+	var ret int64
+	if err := binary.Read(in, binary.LittleEndian, &ret); err != nil {
+		panic(err)
+	}
+	return ret
+}
+
+func read_byte(in io.Reader) uint8 {
+	var ret [1]byte
+	if _, err := io.ReadFull(in, ret[:]); err != nil {
+		panic(err)
+	}
+	return ret[0]
+}
+
+// The reverse of `Lib/importlib/_bootstrap_external.py`
+// _code_to_timestamp_pyc() and _code_to_hash_pyc().
+func Disassemble(in io.Reader, out io.Writer) (err error) {
+	defer func() {
+		if _err := derror.PanicToError(recover()); _err != nil {
+			err = _err
+		}
+	}()
+
+	fmt.Fprintf(out, "(4) magic    = %#08x\n", read_u32le(in))
+
+	flags := read_u32le(in)
+	fmt.Fprintf(out, "(4) flags    = %#032b\n", flags)
+	fmt.Fprintf(out, "             :   [          undefined         ][]\n")
+	fmt.Fprintf(out, "             :                                 ^invalidation mode\n")
+	switch flags & 0b11 {
+	case 0b00:
+		fmt.Fprintf(out, "             : invalidation_mode=TIMESTAMP\n")
+		mtime := read_u32le(in)
+		fmt.Fprintf(out, "(4) src_mtime= %d (%v)\n", mtime, time.Unix(int64(mtime), 0))
+		fmt.Fprintf(out, "(4) src_size = %d\n", read_u32le(in))
+	case 0b01:
+		fmt.Fprintf(out, "             : invalidation_mode=UNCHECKED_HASH\n")
+		var hash [8]byte
+		if _, err := io.ReadFull(in, hash[:]); err != nil {
+			return err
+		}
+		fmt.Fprintf(out, "(8) src_hash = %s\n", hex.EncodeToString(hash[:]))
+	case 0b11:
+		fmt.Fprintf(out, "             : invalidation_mode=CHECKED_HASH\n")
+		var hash [8]byte
+		if _, err := io.ReadFull(in, hash[:]); err != nil {
+			return err
+		}
+		fmt.Fprintf(out, "(8) src_hash = %s\n", hex.EncodeToString(hash[:]))
+	case 0b10:
+		fmt.Fprintf(out, "             : invalidation_mode=INVALID\n")
+		var data [8]byte
+		if _, err := io.ReadFull(in, data[:]); err != nil {
+			return err
+		}
+		fmt.Fprintf(out, "(8) ???????? = 0x%s\n", hex.EncodeToString(data[:]))
+	}
+	return Unmarshal(in, out)
+}
+
+// `Python/marshal.c` read_object().
+func Unmarshal(in io.Reader, out io.Writer) (err error) {
+	defer func() {
+		if _err := derror.PanicToError(recover()); _err != nil {
+			err = _err
+		}
+	}()
+
+	return rObject("", in, out)
+}
+
+var typ2name = map[uint8]string{
+	'0': "TYPE_NULL",
+	'N': "TYPE_NONE",
+	'F': "TYPE_FALSE",
+	'T': "TYPE_TRUE",
+	'S': "TYPE_STOPITER",
+	'.': "TYPE_ELLIPSIS",
+	'i': "TYPE_INT",
+	'I': "TYPE_INT64",
+	'f': "TYPE_FLOAT",
+	'g': "TYPE_BINARY_FLOAT",
+	'x': "TYPE_COMPLEX",
+	'y': "TYPE_BINARY_COMPLEX",
+	'l': "TYPE_LONG",
+	's': "TYPE_STRING",
+	't': "TYPE_INTERNED",
+	'r': "TYPE_REF",
+	'(': "TYPE_TUPLE",
+	'[': "TYPE_LIST",
+	'{': "TYPE_DICT",
+	'c': "TYPE_CODE",
+	'u': "TYPE_UNICODE",
+	'?': "TYPE_UNKNOWN",
+	'<': "TYPE_SET",
+	'>': "TYPE_FROZENSET",
+
+	//0x80: "FLAG_REF",
+
+	'a': "TYPE_ASCII",
+	'A': "TYPE_ASCII_INTERNED",
+	')': "TYPE_SMALL_TUPLE",
+	'z': "TYPE_SHORT_ASCII",
+	'Z': "TYPE_SHORT_ASCII_INTERNED",
+}
+
+const (
+	TYPE_NULL           = '0'
+	TYPE_NONE           = 'N'
+	TYPE_FALSE          = 'F'
+	TYPE_TRUE           = 'T'
+	TYPE_STOPITER       = 'S'
+	TYPE_ELLIPSIS       = '.'
+	TYPE_INT            = 'i'
+	TYPE_INT64          = 'I'
+	TYPE_FLOAT          = 'f'
+	TYPE_BINARY_FLOAT   = 'g'
+	TYPE_COMPLEX        = 'x'
+	TYPE_BINARY_COMPLEX = 'y'
+	TYPE_LONG           = 'l'
+	TYPE_STRING         = 's'
+	TYPE_INTERNED       = 't'
+	TYPE_REF            = 'r'
+	TYPE_TUPLE          = '('
+	TYPE_LIST           = '['
+	TYPE_DICT           = '{'
+	TYPE_CODE           = 'c'
+	TYPE_UNICODE        = 'u'
+	TYPE_UNKNOWN        = '?'
+	TYPE_SET            = '<'
+	TYPE_FROZENSET      = '>'
+
+	FLAG_REF = 0x80
+
+	TYPE_ASCII                = 'a'
+	TYPE_ASCII_INTERNED       = 'A'
+	TYPE_SMALL_TUPLE          = ')'
+	TYPE_SHORT_ASCII          = 'z'
+	TYPE_SHORT_ASCII_INTERNED = 'Z'
+)
+
+func rFloatStr(indent string, in io.Reader, out io.Writer) (err error) {
+	n := read_byte(in)
+	fmt.Fprintf(out, "%s(1)   flen = %d\n", n)
+	buf := make([]byte, n)
+	if _, err := io.ReadFull(in, buf); err != nil {
+		return err
+	}
+	fmt.Fprintf(out, "%s(% 3d) fstr = %q\n", buf)
+	return nil
+}
+
+func rFloatBin(in io.Reader) ([8]byte, float64) {
+	var buf [8]byte
+	if _, err := io.ReadFull(in, buf[:]); err != nil {
+		panic(err)
+	}
+	var val float64
+	if err := binary.Read(bytes.NewReader(buf[:]), binary.LittleEndian, &val); err != nil {
+		panic(err)
+	}
+	return buf, val
+}
+
+func rObject(indent string, in io.Reader, out io.Writer) (err error) {
+	typ := read_byte(in)
+	flag := (typ & FLAG_REF) != 0
+	typ &^= FLAG_REF
+
+	typName, ok := typ2name[typ]
+	if !ok {
+		return fmt.Errorf("unknown type %[1]d %[1]c", typ)
+	}
+
+	flagStr := "0"
+	if flag {
+		flagStr = "FLAG_REF"
+	}
+	fmt.Fprintf(out, "%[1]s(1) type = %[2]q (%[2]s) | %[3]s\n", indent, typ, flagStr, typName)
+
+	switch typ {
+	case TYPE_NULL:
+		// do nothing
+	case TYPE_NONE:
+		fmt.Fprintf(out, "%s         : val = None\n", indent)
+	case TYPE_STOPITER:
+		fmt.Fprintf(out, "%s         : val = StopIteration\n", indent)
+	case TYPE_ELLIPSIS:
+		fmt.Fprintf(out, "%s         : val = Ellipsis\n", indent)
+	case TYPE_FALSE:
+		fmt.Fprintf(out, "%s         : val = False\n", indent)
+	case TYPE_TRUE:
+		fmt.Fprintf(out, "%s         : val = True\n", indent)
+	case TYPE_INT:
+		fmt.Fprintf(out, "%s(4) val  = %d\n", indent, read_i32le(in))
+	case TYPE_INT64:
+		fmt.Fprintf(out, "%s(8) val  = %d\n", indent, read_i64le(in))
+	case TYPE_FLOAT:
+		fmt.Fprintf(out, "%s    val  =\n", indent)
+		if err := rFloatStr(indent+"    ", in, out); err != nil {
+			return err
+		}
+	case TYPE_BINARY_FLOAT:
+		buf, val := rFloatBin(in)
+		fmt.Fprintf(out, "%s(8) val  = %s (%v)\n", indent, hex.EncodeToString(buf[:]), val)
+	case TYPE_COMPLEX:
+		fmt.Fprintf(out, "%s    real =\n", indent)
+		if err := rFloatStr(indent+"    ", in, out); err != nil {
+			return err
+		}
+		fmt.Fprintf(out, "%s    imag =\n", indent)
+		if err := rFloatStr(indent+"    ", in, out); err != nil {
+			return err
+		}
+	case TYPE_BINARY_COMPLEX:
+		buf, val := rFloatBin(in)
+		fmt.Fprintf(out, "%s    real = %s (%v)\n", indent, hex.EncodeToString(buf[:]), val)
+		buf, val = rFloatBin(in)
+		fmt.Fprintf(out, "%s    imag = %s (%v)\n", indent, hex.EncodeToString(buf[:]), val)
+	case TYPE_STRING:
+		fmt.Fprintf(out, "%s    val  =\n")
+		n := read_i32le(in)
+		if n < 0 {
+			return fmt.Errorf("bad marshal data: bytes object size out of range: %d", n)
+		}
+		fmt.Fprintf(out, "%s(4)     slen = %d\n", indent+"    ", n)
+		buf := make([]byte, n)
+		if _, err := io.ReadFull(in, buf); err != nil {
+			return err
+		}
+		fmt.Fprintf(out, "%s%8ssval = %q\n", indent+"    ", fmt.Sprintf("(%d)", n), buf)
+	case TYPE_ASCII_INTERNED, TYPE_ASCII, TYPE_SHORT_ASCII_INTERNED, TYPE_SHORT_ASCII:
+		fmt.Fprintf(out, "%s    val  =\n")
+		isInterned := (typ == TYPE_ASCII_INTERNED) || (typ == TYPE_SHORT_ASCII_INTERNED)
+		var n int32
+		if (typ == TYPE_ASCII) || (typ == TYPE_ASCII_INTERNED) {
+			n = read_i32le(in)
+			fmt.Fprintf(out, "%s(4)     slen = %d\n", indent+"    ", n)
+		} else {
+			n = int32(read_byte(in))
+			fmt.Fprintf(out, "%s(1)     slen = %d\n", indent+"    ", n)
+		}
+		// TODO
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/datawire/pyc-dis
+
+go 1.17
+
+require github.com/datawire/dlib v1.2.4
+
+require github.com/pkg/errors v0.9.1 // indirect
